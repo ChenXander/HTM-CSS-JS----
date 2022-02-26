330dbe881c8078e7884051525d392a6965069232
ChenXander <cx1197257655@gmail.com>
2022-02-26 08:45:15 +0800

    退出登录功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,6 +1,13 @@
 // 每次调用get或post或ajax的时候会先调用ajaxPrefilter这个函数，
 // 这个函数中我们可以拿到ajax提供的配置对象
 $.ajaxPrefilter(function (options) {
-    // 在发起ajax请求之前统一拼接请求的根路径
-    options.url = 'http://www.liulongbin.top:3007' + options.url;
-})
+  // 在发起ajax请求之前统一拼接请求的根路径
+  options.url = "http://www.liulongbin.top:3007" + options.url;
+
+  // 统一为有权限的接口设置headers请求头
+  if (options.url.indexOf("/my") !== -1) {
+    options.headers = {
+      Authorization: localStorage.getItem("token") || "",
+    };
+  }
+});
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,25 @@
 $(function () {
   // 调用getUserInfo获取用户基本信息
   getUserInfo();
+
+  // 退出登录功能
+  var layer = layui.layer;
+  $("#btnLogout").on("click", function () {
+    // 提示用户是否确认退出
+    layer.confirm(
+      "确定退出登录？",
+      { icon: 3, title: "提示" },
+      function (index) {
+        // 清空本地存储的token
+        localStorage.removeItem("token");
+        // 重新跳转到登录页面
+        location.href = "/login.html";
+
+        // 关闭confirm询问框
+        layer.close(index);
+      }
+    );
+  });
 });
 
 // 获取用户的基本信息
@@ -8,10 +27,10 @@ function getUserInfo() {
   $.ajax({
     method: "GET",
     url: "/my/userinfo",
-    // 请求头配置对象
-    headers: {
-      Authorization: localStorage.getItem("token") || "",
-    },
+    // // 请求头配置对象
+    // headers: {
+    //   Authorization: localStorage.getItem("token") || "",  配置到baseAPI中
+    // },
     success: function (res) {
       if (res.status !== 0) {
         return layui.layer.msg("获取用户信息失败！");
