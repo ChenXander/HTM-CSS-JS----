97f5d6640d75750e4e55813481ad2b7bf8e97173
ChenXander <cx1197257655@gmail.com>
2022-02-26 20:26:30 +0800

    渲染文章列表下拉框选项

--- assets/js/acticle/art_list.js
+++ assets/js/acticle/art_list.js
@@ -1,5 +1,6 @@
 $(function () {
   var layer = layui.layer;
+  var form = layui.form;
 
   // 定义美化时间的过滤器
   template.defaults.imports.dataFormat = function (date) {
@@ -30,6 +31,9 @@ $(function () {
   };
 
   initTable();
+  initCate();
+
+
   // 获取文章列表数据的方法
   function initTable() {
     $.ajax({
@@ -43,6 +47,24 @@ $(function () {
         // 使用模板引擎渲染页面的数据
         var htmlStr = template("tpl-table", res);
         $("tbody").html(htmlStr);
+      },
+    });
+  }
+
+//   初始化文章分类的方法
+  function initCate() {
+    $.ajax({
+      method: "GET",
+      url: "/my/article/cates",
+      success: function (res) {
+        if (res.status !== 0) {
+          return layer.msg("获取分类数据失败！");
+        }
+        //   调用模板引擎渲染分类的可选项
+        var htmlStr = template("tpl-cate", res);
+        $("[name = cate_id]").html(htmlStr);
+        // 通过layui重新渲染表单区域的ui结构
+        form.render();
       },
     });
   }
