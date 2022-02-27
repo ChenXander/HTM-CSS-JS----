37d77f830e1f57a7fe70e055b3bd1f65f44e5948
ChenXander <cx1197257655@gmail.com>
2022-02-27 13:33:15 +0800

    文章列表渲染分页

--- assets/js/acticle/art_list.js
+++ assets/js/acticle/art_list.js
@@ -1,6 +1,7 @@
 $(function () {
   var layer = layui.layer;
   var form = layui.form;
+  var laypage = layui.laypage;
 
   // 定义美化时间的过滤器
   template.defaults.imports.dataFormat = function (date) {
@@ -33,7 +34,6 @@ $(function () {
   initTable();
   initCate();
 
-
   // 获取文章列表数据的方法
   function initTable() {
     $.ajax({
@@ -47,11 +47,13 @@ $(function () {
         // 使用模板引擎渲染页面的数据
         var htmlStr = template("tpl-table", res);
         $("tbody").html(htmlStr);
+        // 调用渲染分页的方法
+        renderPage(res.total);
       },
     });
   }
 
-//   初始化文章分类的方法
+  //   初始化文章分类的方法
   function initCate() {
     $.ajax({
       method: "GET",
@@ -69,16 +71,47 @@ $(function () {
     });
   }
 
-//   为删选表单绑定submit事件
-  $('#form-search').on('submit', function(e){
-    e.preventDefault()
+  //   为删选表单绑定submit事件
+  $("#form-search").on("submit", function (e) {
+    e.preventDefault();
     // 获取表单中选中项的值
-    var cate_id = $('[name = cate_id]').val()
-    var state = $('[name = state]').val()
+    var cate_id = $("[name = cate_id]").val();
+    var state = $("[name = state]").val();
     // 为查询参数对象q中的对应的属性赋值
-    q.cate_id = cate_id
-    q.state = state
+    q.cate_id = cate_id;
+    q.state = state;
     // 根据最新的筛选条件，重新渲染表格的数据
-    initTable()
-  })
+    initTable();
+  });
+
+  //   定义渲染分页的方法
+  function renderPage(total) {
+    // 调用 laypage.render() 方法来渲染分页的结构
+    laypage.render({
+      elem: "pageBox", // 分页容器的 Id
+      count: total, // 总数据条数
+      limit: q.pagesize, // 每页显示几条数据
+      curr: q.pagenum, // 设置默认被选中的分页
+      layout: ['count', 'limit', 'prev', 'page', 'next', 'skip'],
+      limits: [2, 3, 5, 10],
+      // 分页发生切换的时候，触发 jump 回调
+      /* 触发 jump 回调的方式有两种：
+                1. 点击页码的时候，会触发 jump 回调
+                2. 只要调用了 laypage.render() 方法，就会触发 jump 回调 */
+      jump: function (obj, first) {
+        /* 可以通过 first 的值，来判断是通过哪种方式，触发的 jump 回调
+        如果 first 的值为 true，证明是方式2触发的
+        否则就是方式1触发的 */
+        // 把最新的页码值，赋值到 q 这个查询参数对象中
+        q.pagenum = obj.curr;
+        // 把最新的条目数，赋值到 q 这个查询参数对象的 pagesize 属性中
+        q.pagesize = obj.limit;
+        // 根据最新的 q 获取对应的数据列表，并渲染表格
+        // initTable()
+        if (!first) {
+            initTable();
+        }
+      },
+    });
+  }
 });
